53ddc8cbe477c75f7e42c1507e97ea075814d38e
daguiheso <daguiheso@gmail.com>
2017-11-05 09:07:56 -0500

    Configure express and Angular 4 deploy  with gcloud

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// Start the server
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+  console.log('Press Ctrl+C to quit.');
+});
